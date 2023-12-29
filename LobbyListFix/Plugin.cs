77643cb5932a16b15ffc736bcc7695d7bc440a0b
echoman_ <jancso.jonatan@gmail.com>
2023-12-29 13:42:27 +0100

    feature/lcllf-1: project setup; patch lobby list transform: expand scrollview content to fit listed servers  On branch feature/lcllf-1  Changes to be committed: 	new file:   .gitignore 	new file:   LobbyListFix.sln 	new file:   LobbyListFix/LobbyListFix.csproj 	new file:   LobbyListFix/Patches/SteamLobbyManagerPatch.cs 	new file:   LobbyListFix/Plugin.cs

---
+++ LobbyListFix/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LobbyListFix.Patches;
+
+namespace LobbyListFix
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "io.github.echoman-dev.LCLobbyListFix";
+        private const string modName = "Lethal Company Lobby List Fix";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static Plugin Instance;
+
+        internal ManualLogSource logSource;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            logSource.LogInfo("Lobby List Fix Mod loaded.");
+
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(SteamLobbyManagerPatch));
+        }
+    }
+}
